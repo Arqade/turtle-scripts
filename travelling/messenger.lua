03d85bb64b67995db1634875dbcaa5b5ca032ea1
Yves Stadler <mvyonline@gmail.com>
2013-05-17 13:27:52 +0200

    Added APIs and travelling script

---
+++ travelling/messenger.lua
@@ -0,0 +1,23 @@
+-- Makes a turtle travel
+
+local args = { ... } 
+
+os.loadAPI("/lib/coords")
+
+local home = coords.getHome()
+
+if home == nil then
+    print("No home set, please input x coord:")
+    local x = read()
+    print("y coord:")
+    local y = read()
+    print("z coord:")
+    local z = read()
+
+    local vector = new vector(x, y, z)
+
+    coords.setHome(vector)
+else
+    print("Turtle home set to : " .. home.x .. "/" .. home.y .. "/" .. home.z)
+end
+
