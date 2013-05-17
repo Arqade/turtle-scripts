0e66f4513962abfe31b57c33ee621aeb32a00fd9
Yves Stadler <mvyonline@gmail.com>
2013-05-17 16:03:34 +0200

    Pathfinding first draft

--- travelling/messenger.lua
+++ travelling/messenger.lua
@@ -1,11 +1,30 @@
 -- Makes a turtle travel
 flyingAlt = 100
 
+function reach(v)
+    local curr = libcoords.getCurrentCoords()
+
+    if v[1] == curr[1] and v[2] == curr[2] then
+        print("Destination is current")
+        libcoords.land()
+    else
+        print("Reaching flying altitude: " .. flyingAlt)
+        libcoords.takeoff(flyingAlt)
+
+        print("Moving to X/Y")
+        coords.pathfindingXthenY(v[1], v[2])
+
+        print("Getting back to land")
+        libcoords.land()
+    end
+end
+
 local args = { ... } 
 
 os.loadAPI("/lib/libcoords")
 
 local home = libcoords.getHome()
+local ori = libcoords.getOrientation()
 
 if home == nil then
     print("No home set, please input x coord:")
@@ -18,15 +37,27 @@ if home == nil then
     local t = {x, y, z} 
 
     libcoords.setHome(t)
-else
-    print("Turtle home set to : " .. home[1] .. "/" .. home[2] .. "/" .. home[3])
-    local curr = libcoords.getCurrentCoords()
-    print("Current position is : " .. curr[1] .. "/" .. curr[2] .. "/" .. curr[3])
-
-    print("Going up to " .. flyingAlt)
-    libcoords.takeoff(flyingAlt)
-    print("Getting back to land")
-    libcoords.land()
-    
 end
+
+if ori == nil then
+    print("Which direction am I facing? north 1, east 2, south 3, west 4")
+    o = read()
+    libcoords.setOrientation(o)
+end
+
+print("Turtle home set to : " .. home[1] .. "/" .. home[2] .. "/" .. home[3])
+local curr = libcoords.getCurrentCoords()
+print("Current position is : " .. curr[1] .. "/" .. curr[2] .. "/" .. curr[3])
+local ori = libcoords.getOrientation()
+print("Turtle is facing : " .. ori)
+
+print("Please input dest x :")
+local xdest = read()
+
+print("Please input dest y :")
+local ydest = read()
+
+local dest = {xdest, ydest}
+
+reach(dest)
 
