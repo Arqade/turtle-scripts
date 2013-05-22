e867c74e0f5e9d5cf1e61553d448b4e219000963
Yves Stadler <mvyonline@gmail.com>
2013-05-22 16:44:57 +0200

    Drop orientation

--- tools/drop.lua
+++ tools/drop.lua
@@ -1,4 +1,20 @@
+local args = { ... }
+
+if #args < 1 then
+    ori = "front"
+else 
+    ori = args[1]
+end
+
 for i = 1, 16 do
     turtle.select(i)
-    turtle.drop()
+    if ori == "front" then
+        turtle.drop()
+    elseif ori = "down" then
+        turtle.dropDown()
+    elseif ori = "up" then
+        turtle.dropUp()
+    else
+        print("Wrong parameter")
+    end
 end
