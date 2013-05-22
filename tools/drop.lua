4ef2a61f3eabbd8f7b1f71469e0dd77c6bac2bae
Yves Stadler <mvyonline@gmail.com>
2013-05-22 16:45:56 +0200

    Corrected ==

--- tools/drop.lua
+++ tools/drop.lua
@@ -10,9 +10,9 @@ for i = 1, 16 do
     turtle.select(i)
     if ori == "front" then
         turtle.drop()
-    elseif ori = "down" then
+    elseif ori == "down" then
         turtle.dropDown()
-    elseif ori = "up" then
+    elseif ori == "up" then
         turtle.dropUp()
     else
         print("Wrong parameter")
